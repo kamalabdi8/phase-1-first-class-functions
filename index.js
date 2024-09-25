2231b665cbcc2f075ecc9238ed7e59768d3a4913
kamalabdi8 <kamalabdi042@gmail.com>
2024-09-25 19:04:35 +0300

    first class

---
+++ index.js
@@ -0,0 +1,11 @@
+function receivesAFunction(callback){
+    callback()
+}
+function returnsANamedFunction(){
+    return function namedFunction(){
+        console.log(`I am a named function!`);
+    }
+}
+function returnsAnAnonymousFunction(){
+    return function(){}
+}
